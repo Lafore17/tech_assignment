a21532f2b2a5b94ca5b26532e5621fb77e88b808
Lafore17 <dkoutun@aristeksystems.com>
2021-06-14 10:57:05 +0300

    feat: prepare app to be deployed

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
 
 const app = express();
@@ -6,6 +7,8 @@ const port = 8080;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "build")));
 
 let intermediaries = [
   {
@@ -105,6 +108,10 @@ app.delete("/products/:id", (req, res) => {
   }, []);
 
   res.sendStatus(200);
+});
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(port);
